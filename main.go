b236f56cf72b93334f7cdea4cc19dd721f6db092
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-08-02 13:13:42 +0100

    read and display questions

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		print("Question: ")
+		scanner.Scan()
+		question := scanner.Text()
+		if question == "exit" {
+			break
+		}
+	}
 }
